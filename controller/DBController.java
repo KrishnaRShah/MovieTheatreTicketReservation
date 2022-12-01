4121f61fa515347b924faab478e71e6152aa196c
KrishnaRShah <krishnashah14957@gmail.com>
2022-12-01 12:13:21 -0700

    Controller stuff

---
+++ controller/DBController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class DBController {
+    private Connection connector;
+
+    String DB_URL;
+    String DB_Username;
+    String DB_Pass;
+
+    private static DBController instance;
+
+}
