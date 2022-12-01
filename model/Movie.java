6caa6b71271ddd57e09223d21eb24ba50ea4b2bc
Manraj Singh <74936295+ManrajSingh6@users.noreply.github.com>
2022-12-01 10:10:48 -0700

    Initial commit. Add all model files

---
+++ model/Movie.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Movie {
+    private ArrayList<Showtime> showtimes;
+    private String movieName;
+    private double movieLength;
+
+    public ArrayList<Showtime> getShowtimes(){
+        return showtimes;
+    }
+
+    public String getMovieName(){
+        return movieName;
+    }
+
+    public double getMovieLength(){
+        return movieLength;
+    }
+
+    public void addShowtime(Showtime showtime){
+        showtimes.add(showtime);
+    }
+
+    public void setMovieName(String name){
+        this.movieName = name;
+    }
+
+    public void setMovieLength(double length){
+        this.movieLength = length;
+    }
+
+}
